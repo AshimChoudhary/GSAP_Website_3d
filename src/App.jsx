c91c43ab87c014eb9b9b01c7f3b46235343b0ece
AshimChoudhary <choudharyashim99@gmail.com>
2025-12-17 16:18:31 +0530

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import Hero from "./components/Hero";
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-robert-medium text-violet-300 underline ">
-      Hello world!
-    </h1>
-  )
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+    </main>
+  );
 }
