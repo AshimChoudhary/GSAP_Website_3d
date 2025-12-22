2327fb69d5ebdb867c4cf00c8b33b94d4c33f465
AshimChoudhary <choudharyashim99@gmail.com>
2025-12-22 17:12:57 +0530

    Added animated navbar with GSAP and music playback option

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <main className="relative min-h-screen w-screen overflow-x-hidden">
+    <main className="relative min-h-screen w-screen overflow-x-hidden ">
       <Navbar />
 
       <Hero />
