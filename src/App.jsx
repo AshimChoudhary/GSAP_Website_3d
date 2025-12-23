08e0a9fddcd524c583d3d02399f07093f220eaed
AshimChoudhary <choudharyashim99@gmail.com>
2025-12-23 20:03:00 +0530

    Adds Features section to main app layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
+import Features from "./components/Features";
 
 export default function App() {
   return (
@@ -11,6 +12,8 @@ export default function App() {
       <Hero />
 
       <About />
+
+      <Features />
     </main>
   );
 }
