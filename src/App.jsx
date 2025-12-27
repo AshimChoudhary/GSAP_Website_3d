e97a66040379598820e6922714d1e36ab409b7bf
AshimChoudhary <choudharyashim99@gmail.com>
2025-12-27 13:06:23 +0530

    Contact Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import About from "./components/About";
 import Navbar from "./components/Navbar";
 import Features from "./components/Features";
 import Story from "./components/Story";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
@@ -17,6 +19,10 @@ export default function App() {
       <Features />
 
       <Story />
+
+      <Contact />
+
+      <Footer />
     </main>
   );
 }
